513aadf6f148c32febdb83e46f5f2b09cf1f0299
Bugg4 <bulgarelli.marco99@gmail.com>
2025-08-20 16:46:21 +0200

    Update trackers.sh

--- trackers.sh
+++ trackers.sh
@@ -1,4 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
-curl -s https://raw.githubusercontent.com/ngosang/trackerslist/refs/heads/master/trackers_best.txt | wl-copy
-echo "trackers_best copied to clipboard!"
+mode=$1
+
+if [ -z "$mode" ]; then
+    echo "missing mode: use 'all' or 'best'"
+    exit 1
+fi
+
+curl -s "https://raw.githubusercontent.com/ngosang/trackerslist/refs/heads/master/trackers_${mode}.txt" | wl-copy
+echo "${mode} trackers copied to clipboard!"
